2d67cbf6925e19973deb255b9988d77fee89ce6e
itheima <itheima@itcast.cn>
2019-06-04 15:26:15 +0800

    测试git提交代码

---
+++ testgitproject/src/main/java/com/itheima/test/TestDemo.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+/**
+ * @author fyj
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("test git project");
+    }
+}
