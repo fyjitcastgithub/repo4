375ac6c6824d9be8bdcbd3062ab45c8dce3d547d
fyjitcastgithub <719669753@qq.com>
2019-06-10 21:49:38 +0800

    修改了内容后提交

---
+++ git-test/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author fyj
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("==================>");
+    }
+
+    public void testSave(){
+        System.out.println("6666666666666666");
+    }
+}
